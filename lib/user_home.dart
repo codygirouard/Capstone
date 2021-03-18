896bb71d8e44ca23910da1807d0f1a5adda30d52
theprankmaster413 <43733505+theprankmaster413@users.noreply.github.com>
2021-03-18 11:35:14 -0500

    Update user_home.dart

--- lib/user_home.dart
+++ lib/user_home.dart
@@ -1,34 +1,66 @@
 import 'package:flutter/material.dart';
-import 'bottom_bar.dart';
+import 'package:flutter_app1/src/widgets/container_home_profile.dart';
+import 'package:flutter_app1/src/widgets/container_home_user.dart';
 
-void main() {
-  runApp(UserHome());
-}
+import '../../calendar.dart';
+import '../../notifications.dart';
 
-class UserHome extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _UserHome();
 }
 
-class _UserHome extends State<UserHome> {
+class _UserHome extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> widgetOptions = [
+    HomeContainerUser(),
+    HomeContainerProfile(), //ANOTHER WIDGETS
+    CalendarUI(),
+    Notifications(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40.0),
-        child: AppBar(
-          backgroundColor: Colors.blue,
-          automaticallyImplyLeading: false,
-          title: const Text('Home'),
-        ),
+      body: widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+            backgroundColor: Colors.blue,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        //selectedItemColor: Colors.redAccent[300],
+        elevation: 10,
+        iconSize: 25,
+        selectedFontSize: 18,
+        onTap: _onItemTapped,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[],
-        ),
-      ),
-      bottomNavigationBar: BottomBar(),
     );
   }
 }
