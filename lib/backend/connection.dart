75368ace072d8735ad75b2d0ea9e703c004d869c
RaghibA <raghibaftab23@gmail.com>
2021-04-06 16:03:34 -0500

    added create user, find by id & username

--- lib/backend/connection.dart
+++ lib/backend/connection.dart
@@ -1,6 +1,8 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
-dbConnect() async {
+// Create User
+createUser(String name, String username, int height, int weight, int age,
+    String password, int streak) async {
   Db db = await Db.create(
       "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
   await db.open();
@@ -8,17 +10,53 @@ dbConnect() async {
   DbCollection users = db.collection('users');
 
   await users.insert({
-    'name': 'Test Two',
-    'username': 'test1',
-    'height': 72,
-    'weight': 188,
-    'age': 25,
-    'password': 'hashedpass',
-    'streak': 4
+    'name': name,
+    'username': username,
+    'height': height,
+    'weight': weight,
+    'age': age,
+    'password': password,
+    'streak': streak
   });
+
+  print('Closing MongoDB');
+  await db.close();
 }
 
+// Find User
+findUserById(String id) async {
+  Db db = await Db.create(
+      "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
+  await db.open();
+  print("Connected to MongoDB");
+  DbCollection users = db.collection('users');
+
+  var user = await users.find(where.id(ObjectId.parse(id))).toList();
+  print(user);
+
+  print('Closing MongoDB');
+  await db.close();
+}
+
+findUserByUsername(String username) async {
+  Db db = await Db.create(
+      'mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz');
+  await db.open();
+  print('Connected to MongoDb');
+  DbCollection users = db.collection('users');
+
+  var user = await users.find(where.eq('username', username)).toList();
+  print(user);
+
+  print('Closing MongoDB');
+  await db.close();
+}
+
+// Update User
+//updateUser() {}
 main(List<String> args) {
-  dbConnect();
+  createUser('Test One', 'Test1', 62, 125, 28, '1234', 17);
+  findUserByUsername('Test1');
+  findUserById('606ccc2e1e55117067049ba3');
   return;
 }
