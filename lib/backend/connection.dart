0442fd9b87cdbd6929ef3b634a31c7c3d2d686b2
RaghibA <raghibaftab23@gmail.com>
2021-04-08 00:13:29 -0500

    Added all CRUD methods for users

--- lib/backend/connection.dart
+++ lib/backend/connection.dart
@@ -1,5 +1,7 @@
 import 'package:mongo_dart/mongo_dart.dart';
 
+//! USER CRUD METHODS
+
 // Create User
 createUser(String name, String email, int height, int weight, int age,
     String password, int streak) async {
@@ -69,7 +71,7 @@ findUserByEmail(String email) async {
   return (user);
 }
 
-// authenticate user login
+// Authenticate user login
 authenticateUser(String email, String password) async {
   bool authenticated = false;
 
@@ -97,12 +99,51 @@ authenticateUser(String email, String password) async {
 }
 
 // Update User
-//updateUser() {}
+updateUser(String id, String name, String email, int height, int weight,
+    int age, String password, int streak) async {
+  Db db = await Db.create(
+      "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
+  await db.open();
+  print("Connected to MongoDB");
+  DbCollection users = db.collection('users');
+
+  await users.update(await users.findOne(where.id(ObjectId.parse(id))), {
+    'name': name,
+    'email': email,
+    'password': password,
+    'height': height,
+    'weight': weight,
+    'age': age,
+    'streak': streak
+  });
+
+  print('Closing MongoDB');
+  await db.close();
+}
+
+// Delete User
+deleteUser(String id) async {
+  Db db = await Db.create(
+      "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
+  await db.open();
+  print("Connected to MongoDB");
+  DbCollection users = db.collection('users');
+
+  await users.remove(where.id(ObjectId.parse(id)));
+  print('User (_id: ${id}) removed.');
+
+  print('Closing MongoDB');
+  await db.close();
+}
+//! END OF USER CRUD METHODS
+
 main(List<String> args) {
-  createUser('Test 3', 'test3@email.com', 62, 125, 28, '1234', 17);
+  // createUser('Test 2', 'test2@email.com', 72, 175, 24, '1234', 32);
   // findUserByEmail('Test1');
   // findUserById('606ccc2e1e55117067049ba3');
-  var auth = authenticateUser('test3@email.com', '1234');
-  print(auth);
+  // var auth = authenticateUser('test3@email.com', '1234');
+  // print(auth);
+  // updateUser('606e0a407f314f0c108446f3', 'test 1 update', 'test@test.com', 175, 160, 40, '1234', 12);
+  // deleteUser('606e909ff039883a1d95f0b8');
   return;
 }
