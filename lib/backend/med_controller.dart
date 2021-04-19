7a5a26b1498143c93f052954e549ac1e5c31ebad
RaghibA <raghibaftab23@gmail.com>
2021-04-19 00:25:18 -0500

    med methods

--- lib/backend/med_controller.dart
+++ lib/backend/med_controller.dart
@@ -2,8 +2,7 @@ import 'package:mongo_dart/mongo_dart.dart';
 import 'models/users.dart';
 import 'models/medicine.dart';
 
-//! Medicine
-// add medicine
+//! add medicine
 addMedicine(
     String userId, String name, String dosage, int time, int repeat) async {
   Db db = await Db.create(
@@ -25,6 +24,7 @@ addMedicine(
   await db.close();
 }
 
+//! get medicine by user ID
 getUserMedicine(String userId) async {
   Db db = await Db.create(
       "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
@@ -38,4 +38,35 @@ getUserMedicine(String userId) async {
   print('Closing MongoDB');
   await db.close();
   return (userMeds);
+}
+
+//! update medicine
+updateMed(String medId) async {
+  Db db = await Db.create(
+      "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
+  await db.open();
+  print("Connected to MongoDB");
+  DbCollection meds = db.collection('medicine');
+
+  await meds.update(await meds.findOne(where.id(ObjectId.parse(medId))), {
+    // reassign med attributes
+  });
+
+  print('Closing MongoDB');
+  await db.close();
+}
+
+//! Delete medicine
+deleteMedicine(String medId) async {
+  Db db = await Db.create(
+      "mongodb+srv://user:teamultragroup@cluster0.rbbqs.mongodb.net/ultramedz");
+  await db.open();
+  print("Connected to MongoDB");
+  DbCollection meds = db.collection('medicine');
+
+  await meds.remove(where.id(ObjectId.parse(medId)));
+  print('User (_id: ${medId}) removed.');
+
+  print('Closing MongoDB');
+  await db.close();
 }
