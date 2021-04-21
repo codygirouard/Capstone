42ccb7846ef202e4881b6d16ba884e41f5f229a8
Cody Girouard <codyagirouard@gmail.com>
2021-04-20 23:31:59 -0500

    Calendar page now pulls directly from database

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'bottom_bar.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'backend/med_controller.dart';
+import 'globals.dart' as globals;
 
+// constants
 const double kImagesSize = 40.0;
 const Color kPrimaryColor = Colors.blue;
 const Color kSecondaryColor = Colors.blueGrey;
@@ -11,22 +13,8 @@ void main() {
   initializeDateFormatting().then((_) => runApp(Calendar()));
 }
 
-class Calendar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calendar',
-      theme: ThemeData(
-        primarySwatch: kPrimaryColor,
-      ),
-      home: CalendarUI(title: 'Calendar'),
-    );
-  }
-}
-
-class CalendarUI extends StatefulWidget {
-  CalendarUI({Key key, this.title}) : super(key: key);
+class Calendar extends StatefulWidget {
+  Calendar({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -34,7 +22,7 @@ class CalendarUI extends StatefulWidget {
   _CalendarUI createState() => _CalendarUI();
 }
 
-class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
+class _CalendarUI extends State<Calendar> with TickerProviderStateMixin {
   Map<DateTime, List> _events;
   List _selectedEvents;
   AnimationController _animationController;
@@ -43,76 +31,38 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _events = {
-      DateTime.utc(2021, 3, 2): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM',
-        'Levothyroxine 9:00 PM',
-        'Azithromycin 9:00 PM'
-      ],
-      DateTime.utc(2021, 3, 9): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 16): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 23): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 30): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 4): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 3, 11): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 3, 18): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 3, 25): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 3, 6): ['Simvastatin 11:00 AM', 'Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 3, 13): [
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 20): [
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 3, 27): [
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 2, 23): [
-        'Vicodin 10:00 AM',
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 2, 25): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 2, 27): [
-        'Simvastatin 11:00 AM',
-        'Lisinopril 11:00 AM'
-      ],
-      DateTime.utc(2021, 4, 1): ['Lisinopril 11:00 AM'],
-      DateTime.utc(2021, 4, 3): ['Simvastatin 11:00 AM', 'Lisinopril 11:00 AM'],
-    };
+    loadMedicine();
+    _events = Map<DateTime, List>();
+    DateTime now = DateTime.now();
 
-    _selectedEvents = _events[DateTime.utc(
-            DateTime.now().year, DateTime.now().month, DateTime.now().day)] ??
-        [];
+    _selectedEvents = _events[DateTime.utc(now.year, now.month, now.day)] ?? [];
     _calendarController = CalendarController();
 
     _animationController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 400),
+      duration: const Duration(milliseconds: 400),
     );
 
     _animationController.forward();
+  }
+
+  void loadMedicine() async {
+    var meds = await getUserMedicine(globals.id);
+    List<List<String>> medsList = [];
+    var i = 0;
+    for (i = 0; i < meds.length; i++) {
+      List<String> med = [];
+      med.add(meds[i]['name']);
+      med.add(meds[i]['repeat'].toString());
+      med.add(meds[i]['time'].toString());
+      medsList.add(med);
+    }
+    setState(() {
+      _events = getMedicines(medsList);
+      DateTime now = DateTime.now();
+      _selectedEvents =
+          _events[DateTime.utc(now.year, now.month, now.day)] ?? [];
+    });
   }
 
   @override
@@ -133,7 +83,8 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 40.0,
-        title: Text(widget.title),
+        leading: new Container(),
+        title: Text('Calendar'),
         centerTitle: true,
         actions: [
           Icon(Icons.calendar_today),
@@ -146,12 +97,11 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
         mainAxisSize: MainAxisSize.max,
         children: [
           _buildTableCalendarWithBuilders(),
-          SizedBox(height: 8.0),
-          SizedBox(height: 8.0),
+          const SizedBox(height: 8.0),
+          const SizedBox(height: 8.0),
           Expanded(child: _buildEventList()),
         ],
       ),
-      bottomNavigationBar: BottomBar(),
     );
   }
 
@@ -164,7 +114,7 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
       formatAnimation: FormatAnimation.slide,
       startingDayOfWeek: StartingDayOfWeek.monday,
       availableGestures: AvailableGestures.all,
-      availableCalendarFormats: {
+      availableCalendarFormats: const {
         CalendarFormat.month: '',
       },
       calendarStyle: CalendarStyle(
@@ -185,8 +135,8 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
           return FadeTransition(
             opacity: Tween(begin: 0.0, end: 1.0).animate(_animationController),
             child: Container(
-              margin: EdgeInsets.all(4.0),
-              padding: EdgeInsets.only(top: 5.0, left: 6.0),
+              margin: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.only(top: 5.0, left: 6.0),
               color: kPrimaryColor,
               width: 100,
               height: 100,
@@ -200,8 +150,8 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
         },
         todayDayBuilder: (context, date, _) {
           return Container(
-            margin: EdgeInsets.all(4.0),
-            padding: EdgeInsets.only(top: 5.0, left: 6.0),
+            margin: const EdgeInsets.all(4.0),
+            padding: const EdgeInsets.only(top: 5.0, left: 6.0),
             color: kPrimaryColor.withOpacity(0.4),
             width: 100,
             height: 100,
@@ -236,7 +186,7 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
 
   Widget _buildEventsMarker(DateTime date, List events) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 300),
       decoration: BoxDecoration(
         shape: BoxShape.rectangle,
         color: _calendarController.isSelected(date)
@@ -267,17 +217,13 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
                   border: Border.all(width: 0.8),
                   borderRadius: BorderRadius.circular(12.0),
                 ),
-                margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
                 child: ListTile(
                   leading: Image.asset(
                     'images/Pill.jpg',
                     height: kImagesSize,
                     width: kImagesSize,
-                  ),
-                  trailing: Icon(
-                    Icons.check,
-                    color: Colors.green,
-                    size: kImagesSize,
                   ),
                   title: Text(event.toString()),
                   horizontalTitleGap: 30.0,
@@ -285,5 +231,92 @@ class _CalendarUI extends State<CalendarUI> with TickerProviderStateMixin {
               ))
           .toList(),
     );
+  }
+
+  // convert medicine data into format that can be inserted into the calendar
+  Map<DateTime, List<String>> getMedicines(List<List<String>> meds) {
+    Map<DateTime, List<String>> events = {};
+    for (final v in meds) {
+      String name = v[0];
+      int repeat = int.parse(v[1]);
+      String time = v[2];
+      time = getTime(time);
+
+      if (repeat != null) {
+        // is days a number?
+        DateTime now = DateTime.now();
+        DateTime today = DateTime.utc(now.year, now.month, now.day);
+        events = addEvent(events, today, name + " " + time);
+        for (int i = 1; i < 60; i++) {
+          var date = today.add(Duration(hours: i * repeat));
+          events = addEvent(events, date, name + " " + time);
+          date = today.subtract(Duration(hours: i * repeat));
+          events = addEvent(events, date, name + " " + time);
+        }
+      }
+    }
+
+    // sort all entries in a day by time and then by name
+    events.forEach((k, v) => v.sort((a, b) => compareTimes(a, b)));
+    return events;
+  }
+
+  // convert number time into a string time (e.g.: 1500 = 3:00 PM)
+  String getTime(String time) {
+    int timeNum = int.parse(time);
+    time = (timeNum >= 1200) ? " PM" : " AM";
+    String minutes = (timeNum % 100) < 10
+        ? "0" + (timeNum % 100).toString()
+        : (timeNum % 100).toString();
+    timeNum = timeNum > 1200 ? timeNum - 1200 : timeNum;
+    time = (timeNum ~/ 100).toString() + ":" + minutes + time;
+
+    return time;
+  }
+
+  // add an event to a specific day in events map
+  Map<DateTime, List<String>> addEvent(
+      Map<DateTime, List<String>> events, DateTime date, String event) {
+    if (events.containsKey(date)) {
+      events[date].add(event);
+    } else {
+      events[date] = [event];
+    }
+    return events;
+  }
+
+  // add an event to the events map based on weekdays
+  Map<DateTime, List<String>> addEventByWeekday(
+      Map<DateTime, List<String>> events, String days, String event) {
+    if (days.length < 7) {
+      days = days.substring(0, 2);
+    }
+    return events;
+  }
+
+  // convert string time to number time (e.g.: 12:00 PM = 1200)
+  int strTimeToNum(String time) {
+    int period = time.split(" ")[1] == "AM" ? 0 : 1200;
+    if (time.split(":")[0] == "12") {
+      period = period == 0 ? 1200 : 0;
+    }
+    period += int.parse(time.replaceAll(RegExp(r':'), '').split(" ")[0]);
+    return period;
+  }
+
+  // compare two events to sort (e.g.: MedicineA 9:00 AM < MedicineB 5:00 PM)
+  int compareTimes(String time1, String time2) {
+    var time1Arr = time1.split(" ");
+    var time2Arr = time2.split(" ");
+    String name1 = time1Arr[0];
+    String name2 = time2Arr[0];
+    int timeNum1 = strTimeToNum(time1Arr[1] + " " + time1Arr[2]);
+    int timeNum2 = strTimeToNum(time2Arr[1] + " " + time2Arr[2]);
+
+    int compare = timeNum1.compareTo(timeNum2);
+    if (compare == 0) {
+      compare = name1.compareTo(name2);
+    }
+    return compare;
   }
 }
--- lib/globals.dart
+++ lib/globals.dart
@@ -3,6 +3,6 @@ String name = '';
 String pharmacy = '';
 String insurance = '';
 String phone = '';
+String id = '';
 int height = 0;
 int weight = 0;
-
